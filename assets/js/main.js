8c5366a65bf54d2bf33f8988947679c039ebab9f
tomoaki-kimura <tkim19750404@gmail.com>
2025-10-10 15:13:11 +0900

    modify side menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,10 @@
----
----
+import "./side_column.js"
 
-// ブレイクポイントを設定
-const mediaQuery = window.matchMedia('(min-width: 768px)');
-
-// 関数を定義
-const checkWindow = (windowSize) => {
-    // ウィンドウサイズが768px以上か
-    if (windowSize.matches) {
-        // 768px以上の時（PCの処理）
-        console.log('PC');
-    } else {
-        // 上記以外の時（SPの処理）
-        console.log('SP');
-    }
-}
-
-// ロード時に判定
-checkWindow(mediaQuery);
-
-// ブレイクポイントが切り替わったら判定
-mediaQuery.addEventListener('change', checkWindow);
+const url = new URL(window.location.href)
+const part = url.pathname
+console.log(part)
+const chapter = url.searchParams.get("chapter")
+const posts = document.querySelectorAll(`.${chapter}`)
+const target = document.querySelector('#posts')
+target.innerHTML = ""
+posts.forEach(o => target.append(o))
---
+++ assets/js/side_column.js
@@ -0,0 +1,78 @@
+  window.onload = function() {
+
+    // side_column state
+    const toggleMenuOpen = document.querySelector('.toggle-menu-open')
+    const toggleMenuClose = document.querySelector('.toggle-menu-close')
+    const sideColumn = document.querySelector('aside')
+    const main = document.querySelector('main')
+    const sideColumnOpen = document.querySelector('.side-column-open')
+    const sideColumnClose = document.querySelector('.side-column-close')
+
+    const closeSideColumn = () => {
+      sideColumn.classList.remove('w-64')
+      sideColumn.classList.add('w-9')
+      main.classList.remove('ml-64')
+      main.classList.add('ml-9')
+      main.classList.add('w-screen')
+      sideColumnOpen.classList.add('hidden')
+      sideColumnClose.classList.remove('hidden')
+    }
+
+    const openSideColumn = () => {
+      sideColumn.classList.remove('w-9')
+      sideColumn.classList.add('w-64')
+      main.classList.remove('ml-9')
+      main.classList.add('ml-64')
+      sideColumnClose.classList.add('hidden')
+      sideColumnOpen.classList.remove('hidden')
+    }
+
+    // toggle
+    toggleMenuOpen.addEventListener('click', (e) => {
+      closeSideColumn()
+    })
+    toggleMenuClose.addEventListener('click', (e) => {
+      openSideColumn()
+    })
+
+    // responsive
+    const mediaQuery = window.matchMedia('(min-width: 768px)');
+
+    const checkWindow = (windowSize) => {
+        if (windowSize.matches) {
+            openSideColumn()
+            console.log('PC');
+        } else {
+            closeSideColumn()
+            console.log('SP');
+        }
+    }
+
+    checkWindow(mediaQuery);
+
+    mediaQuery.addEventListener('change', checkWindow);
+
+    //subMenu
+    document.querySelectorAll(".sidebar-dropdown-toggle").forEach((item) => {
+      item.addEventListener("click", (e) => {
+        e.preventDefault();
+        const parent = item.closest(".group");
+        if (parent.hasAttribute("data-state-opened")) {
+          parent.removeAttribute("data-state-opened");
+        } else {
+          document.querySelectorAll(".sidebar-dropdown-toggle").forEach((i) => {
+            i.closest(".group").removeAttribute("data-state-opened");
+          })
+          parent.toggleAttribute("data-state-opened");
+        }
+      })
+    })
+
+    // current_page menu
+    const url = new URL(window.location.href)
+    const part = url.pathname
+    const currentChapter = part.replace('/', '').replace('.html', '')
+    const currentChapterNode = document.querySelector(`a[href='${currentChapter}']`)
+    const currentParentNode = currentChapterNode.parentNode
+    currentParentNode.toggleAttribute("data-state-opened")
+  }
