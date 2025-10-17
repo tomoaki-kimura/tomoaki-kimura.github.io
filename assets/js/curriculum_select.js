737e16392914a78ab47a7bcc95e67b09978fc48b
tomoaki-kimura <tkim19750404@gmail.com>
2025-10-17 14:13:45 +0900

    modify menu

--- assets/js/curriculum_select.js
+++ assets/js/curriculum_select.js
@@ -1,9 +1,23 @@
 const url = new URL(window.location.href)
-const chapter = url.searchParams.get("chapter")
+const currentChapter = url.searchParams.get("chapter")
+const currentPart = url.pathname.replace('.html', '').split('/')[2]
+const posts = document.querySelectorAll(`.post.${currentChapter}`)
+const postsWrapper = document.querySelector('#posts')
 
-const posts = document.querySelectorAll(`.post.${chapter}`)
-const target = document.querySelector('#posts')
-if (target) { 
-  target.innerHTML = ""
-  posts.forEach(o => target.append(o))
+if (postsWrapper) { 
+  postsWrapper.innerHTML = ""
+  posts.forEach(o => {
+    postsWrapper.append(o);
+  })
+}
+
+const pageHeaderTitle = document.querySelector('.params-chapter')
+const currenPartData = CURRICULUMS_DATA.find(o => o.part == currentPart)
+
+if (currenPartData) {
+  const currentPartChapters = currenPartData.contents
+  const currentChapterIndex = currentPartChapters.findIndex(o => o.chapter == currentChapter)
+  const currentChapterData = currentPartChapters[currentChapterIndex]
+  const currentChapterTitle = currentChapterData.title
+  pageHeaderTitle.innerHTML = `${ currentChapterIndex + 1 }章 - ${ currentChapterTitle }`
 }
