29d8d134fc0c7a4a544c54ac791cc6a1ce6e94b1
tomoaki-kimura <tkim19750404@gmail.com>
2025-10-18 21:36:25 +0900

    modify page_turn js

--- _site/assets/js/page_turns.js
+++ _site/assets/js/page_turns.js
@@ -1,3 +1,20 @@
 const chapters = document.querySelectorAll('.chapters .chapter')
-
-console.log(chapters)
+let previous;
+let next;
+if (chapters.length > 1) {
+  const currentPageUrl = new URL(location.href)
+  chapters.forEach((o, i) => {
+    const chapterUrl = new URL(o.href)
+    if (chapterUrl.pathname == currentPageUrl.pathname) {
+      const _previous = chapters[i - 1]
+      const _next = chapters[i + 1]
+      if (_previous) {
+        previous = new URL(_previous)
+      }
+      if (_next) {
+        next = new URL(_next)
+      }
+    }
+  })
+}
+console.log(previous, next)
